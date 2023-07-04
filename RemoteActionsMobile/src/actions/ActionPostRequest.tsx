1e25c680a8371b0575b377c3e07447056e8b3ac7
Hamish <37350715+hamishkendall@users.noreply.github.com>
2023-07-04 15:06:05 +1200

    Added custom timeout

--- RemoteActionsMobile/src/actions/ActionPostRequest.tsx
+++ RemoteActionsMobile/src/actions/ActionPostRequest.tsx
@@ -3,21 +3,32 @@ import ActionType from "./ActionType";
 
 const ActionPostRequest = async (actionTitle: string, action: ActionType) => {
     
+    const timeoutDuration = 3000;
+    const timeoutPromise = new Promise<Response>((_, reject) => {
+        setTimeout(() => {
+            reject(new Error('Request times out'));
+        }, timeoutDuration);
+    });
+
     try {
-        const response = await fetch('http://192.168.1.222:8082/action', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({
-                AuthToken: 'TEMPTOKEN',
-                Action: action,
+        const response = await Promise.race([
+            fetch('http://192.168.1.222:8082/action', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    AuthToken: 'TEMPTOKEN',
+                    Action: action,
+                }),
             }),
-        });
+            timeoutPromise,
+        ]);
+        
         Alert.alert('Action Result: '+response.status, actionTitle + ' completed!');
 
     } catch (error) {
-        Alert.alert('Action Result Failed', actionTitle + ' Failed!' + error)
+        Alert.alert('Action Result Failed', actionTitle + ' Failed! \n' + error)
     }
 }
 
