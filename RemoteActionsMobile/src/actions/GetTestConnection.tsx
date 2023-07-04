156957a48cf6c26356ff4814567cac41fc3eebb4
Hamish <37350715+hamishkendall@users.noreply.github.com>
2023-07-04 15:12:12 +1200

    Added custom timeout to get

--- RemoteActionsMobile/src/actions/GetTestConnection.tsx
+++ RemoteActionsMobile/src/actions/GetTestConnection.tsx
@@ -1,10 +1,24 @@
+import {Alert} from 'react-native';
+
 const GetTestConnection = async () => {
+    
+    const timeoutDuration = 3000;
+    const timeoutPromise = new Promise<Response>((_, reject) => {
+        setTimeout(() => {
+            reject(new Error('Request times out'));
+        }, timeoutDuration);
+    });
+
     try {
-        const response = await fetch(
-          'http://192.168.1.222:8082/action',
-        ).then(r => console.log(r.status));
+        const response = await Promise.race([
+            fetch('http://192.168.1.222:8082/action',),
+            timeoutPromise,
+        ]);
+        
+        Alert.alert('Connection Result: '+response.status, 'Connection Success!');
+
     } catch (error) {
-        console.error(error);
+        Alert.alert('Action Failed', ''+error);
     }
 }
 
